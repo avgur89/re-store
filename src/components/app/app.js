acc4187b987ff406ed457ae141496f94b2ac88f5
alexgurov <avgur89@gmail.com>
2020-06-01 13:48:24 +0300

    Setup project folder structure

---
+++ src/components/app/app.js
@@ -0,0 +1,16 @@
+// Core
+import React, { Component } from 'react';
+
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Welcome to ReStore!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
